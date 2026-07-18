5975517b99c1cefa53a7be4f216e0da1631a25d5
deepgit-hub <darkdeepak001@gmail.com>
2026-07-18 23:57:14 +0530

    added feilds in topic.jsx

--- src/pages/Topic.jsx
+++ src/pages/Topic.jsx
@@ -3,7 +3,6 @@ import topics from "../data/topics";
 
 function Topic() {
   const navigate = useNavigate();
-
   const { id } = useParams();
 
   const topic = topics.find((topic) => topic.id === Number(id));
@@ -14,14 +13,84 @@ function Topic() {
 
       <h1>{topic.title}</h1>
 
-      <h2>English Definition</h2>
+      <hr />
+
+      <h2>📖 English Definition</h2>
       <p>{topic.englishDefinition}</p>
 
-      <h2>Tamil Definition</h2>
+      <hr />
+
+      <h2>📖 Tamil Definition</h2>
       <p>{topic.tamilDefinition}</p>
 
-      <h2>Real World Usage</h2>
+      <hr />
+
+      <h2>🌍 Real World Usage</h2>
       <p>{topic.realWorldUsage}</p>
+
+      <hr />
+
+      <h2>💻 Syntax</h2>
+
+      <pre>
+        <code>{topic.syntax}</code>
+      </pre>
+
+      <hr />
+
+      <h2>📘 Syntax Explanation (English)</h2>
+
+      <ul>
+        {topic.syntaxExplanationEnglish.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <h2>📗 Syntax Explanation (Tamil)</h2>
+
+      <ul>
+        {topic.syntaxExplanationTamil.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <h2>🖥 Example Program</h2>
+
+      <pre>
+        <code>{topic.exampleProgram}</code>
+      </pre>
+
+      <hr />
+
+      <h2>📘 Program Explanation (English)</h2>
+
+      <ul>
+        {topic.programExplanationEnglish.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <h2>📗 Program Explanation (Tamil)</h2>
+
+      <ul>
+        {topic.programExplanationTamil.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <h2>🖥 Output</h2>
+
+      <pre>
+        <code>{topic.output}</code>
+      </pre>
     </>
   );
 }
