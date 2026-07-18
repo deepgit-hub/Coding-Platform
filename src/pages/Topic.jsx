5b1a85f94818d8de249d59563c034a66cf048f36
deepgit-hub <darkdeepak001@gmail.com>
2026-07-18 23:22:36 +0530

    modified tpic

--- src/pages/Topic.jsx
+++ src/pages/Topic.jsx
@@ -1,7 +1,27 @@
+import { useNavigate, useParams } from "react-router-dom";
+import topics from "../data/topics";
+
 function Topic() {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  const topic = topics.find((topic) => topic.id === Number(id));
+
   return (
     <>
-      <h1>Topic Page</h1>
+      <button onClick={() => navigate("/")}>⬅ Back</button>
+
+      <h1>{topic.title}</h1>
+
+      <h2>English Definition</h2>
+      <p>{topic.englishDefinition}</p>
+
+      <h2>Tamil Definition</h2>
+      <p>{topic.tamilDefinition}</p>
+
+      <h2>Real World Usage</h2>
+      <p>{topic.realWorldUsage}</p>
     </>
   );
 }
