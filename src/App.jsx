73d208dc22ceb406856d80a1b76d0658f89c7b23
deepgit-hub <darkdeepak001@gmail.com>
2026-07-18 22:05:32 +0530

    changed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello Deepak 🚀</h1>;
+  return (
+    <div style={{ color: "red", fontSize: "50px" }}>
+      Hello Deepak 🚀
+    </div>
+  );
 }
 
 export default App;
