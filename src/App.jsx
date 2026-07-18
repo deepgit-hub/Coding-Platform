6dc4a4845792051ac970a2ffe34e21fad509381a
deepgit-hub <darkdeepak001@gmail.com>
2026-07-18 22:59:06 +0530

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Topic from "./pages/Topic";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/topic/:id" element={<Topic />} />
+    </Routes>
   );
 }
 
