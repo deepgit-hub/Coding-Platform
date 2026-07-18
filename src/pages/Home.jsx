92b785502e3123f6d1157e0c964bb6fc14b4b365
deepgit-hub <darkdeepak001@gmail.com>
2026-07-18 22:06:42 +0530

    added home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,10 @@ import TopicCard from "../components/TopicCard";
 
 function Home() {
   const topics = [
-    { id: 1, title: "Operators", locked: true },
-    { id: 2, title: "Loops", locked: false },
-    { id: 3, title: "If Else", locked: false },
-    { id: 4, title: "Conditional Operators", locked: false }
+    { id: 1, title: "Operators", locked: false },
+    { id: 2, title: "Loops", locked: true },
+    { id: 3, title: "If Else", locked: true },
+    { id: 4, title: "Conditional Operators", locked: true },
   ];
 
   return (
