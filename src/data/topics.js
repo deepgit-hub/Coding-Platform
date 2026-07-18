ad576e7816026fa80791902ce65e111d58ea6762
deepgit-hub <darkdeepak001@gmail.com>
2026-07-18 22:11:03 +0530

    added topics

---
+++ src/data/topics.js
@@ -0,0 +1,24 @@
+const topics = [
+  {
+    id: 1,
+    title: "Operators",
+    locked: false,
+  },
+  {
+    id: 2,
+    title: "Loops",
+    locked: true,
+  },
+  {
+    id: 3,
+    title: "If Else",
+    locked: true,
+  },
+  {
+    id: 4,
+    title: "Conditional Operators",
+    locked: true,
+  },
+];
+
+export default topics;
